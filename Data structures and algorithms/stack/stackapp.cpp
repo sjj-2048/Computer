b278c21f917c42fd64dbf4fc6bed5725de928f53
miracle2020 <63940636+sjj-2048@users.noreply.github.com>
2020-11-13 16:23:21 +0800

    Rename applicationofstack.cpp to stackapp.cpp

--- Data structures and algorithms/stack/stackapp.cpp
+++ Data structures and algorithms/stack/stackapp.cpp
@@ -1,6 +1,6 @@
 #include"stack.h"
 
-//½øÖÆ×ª»»
+//Â½Ã¸Ã–Ã†Ã—ÂªÂ»Â»
 
 void convert(Stack<char> &S, _int64 n, int base) {
 	static char digit[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
